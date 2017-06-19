e6562782b13cd20ae7d56f8b31789f858ea0804a
bredele <olivier.wietrich@gmail.com>
2017-06-19 11:08:32 -0600

    Call after hook

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ module.exports = function (obj, before, after) {
     get(target, property, receiver) {
       const cb = target[property]
       const beforeHook = before && before[property]
-      //const afterHook = after[property]
+      const afterHook = after && after[property]
       if (typeof cb === 'function') {
         return (...args) => {
           if (typeof beforeHook === 'function') {
             args = [beforeHook.apply(this, args)]
           }
-          return cb.apply(this, args)
+          let result = cb.apply(this, args)
+          if (typeof afterHook === 'function') {
+            result = afterHook.call(this, result)
+          }
+          return result
         }
       }
       return cb
