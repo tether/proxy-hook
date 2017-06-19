df6437aa8e626a1549aa3d790c4565d9860e12f9
bredele <olivier.wietrich@gmail.com>
2017-06-19 11:10:25 -0600

    Hook function calls after

--- test/after.js
+++ test/after.js
@@ -20,3 +20,55 @@ test('after # should hook a function', assert => {
   })
   assert.equal(api.get('world'), 'hello world')
 })
+
+
+
+test('after # should only hook an existing function', assert => {
+  assert.plan(1)
+  const api = hook({
+    get(params) {
+      return params
+    }
+  }, null, {
+    post() {
+      return 'hello'
+    }
+  })
+  assert.equal(api.get('world'), 'world')
+})
+
+
+test('after # should not hook a property', assert => {
+  assert.plan(2)
+  const api = hook({
+    get: 'hello'
+  }, null, {
+    get() {
+      return 'world'
+    }
+  })
+  assert.equal(typeof api.get, 'string')
+  assert.equal(api.get, 'hello')
+})
+
+
+test('after # should work with multiple functions', assert => {
+  assert.plan(2)
+  const api = hook({
+    get(params) {
+      return params
+    },
+    post(params) {
+      return 'hello ' + params
+    }
+  }, null, {
+    get(param) {
+      return `foo and ${param}`
+    },
+    post(params) {
+      return params
+    }
+  })
+  assert.equal(api.get('bar'), 'foo and bar')
+  assert.equal(api.post('world'), 'hello world')
+})
